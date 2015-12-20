34379cfdfb8ea19361e675e8bd3266725f67b298
anbnyc <alecbarrett@gmail.com>
2015-12-19 19:17:26 -0500

    update

--- travel_map.R
+++ travel_map.R
@@ -1,6 +1,17 @@
 library(ggmap)
+library(RCurl)
+library(googlesheets)
+library(jsonlite)
+suppressMessages(library(dplyr))
 
-data <- read.csv("/Users/ANB/Downloads/Travel Recs for E&L - Sheet1.csv")
+responses <- gs_title("MapInYourLap_Responses")
+newdata <- responses %>% gs_read()
+
+cartoapi <- "4feddb054914576135a3d111bb473ce9e2545039"
+query <- "select * from mapinyourlap"
+currentdata <- fromJSON(paste0("https://anbnyc.cartodb.com/api/v2/sql?q=",query,"&api_key=",cartoapi))
+
+
 data$Address <- gsub("\n",",",data$Address)
 geos <- geocode(data$Address)
 data[,c("Lon","Lat")] <- geos
