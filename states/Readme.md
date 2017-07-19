c442440b066f380c6bf5279febc5831d34a188c4
Alec Barrett <alecbarrett@gmail.com>
2017-07-19 15:47:23 -0400

    complete v1 of state map

---
+++ states/Readme.md
@@ -0,0 +1,12 @@
+## Neighboring States
+Produced at [TWO-N](www.two-n.com).
+How many neighbors does each state have? This map reimagines the states as regular polygons with one side for each neighbor. Click a state to see its neighbors up close!
+
+### Methodology
+When you click on a state, the position of its neighbors approximate their true relative geographic position. The relative positions are calculated using the geographic centers of each state.
+
+### Sources
+With inspiration and code from:
+* https://bl.ocks.org/mbostock/4090848
+* https://gist.github.com/conorbuck/2606166
+* https://gist.github.com/davidegazze/56bc005090be06ccf545
--- states/index.js
+++ states/index.js
@@ -1,8 +1,8 @@
-const pi = Math.PI
+const pi = Math.PI;
 var svg = d3.select("svg");
 var path = d3.geoPath();
-// var states = 
-
+const bigStateRadius = 50;
+const littleStateRadius = 20;
 
 d3.queue()
   .defer(d3.json, "https://d3js.org/us-10m.v1.json")
@@ -11,96 +11,119 @@ d3.queue()
     run(results[0], results[1])
   })
 
-//https://bl.ocks.org/mbostock/4090848
 function run(us, states){
 
-  var current
-  var path = d3.geoPath();
+  var current;
   var features = topojson.feature(us, us.objects.states).features;
   features.forEach(e => {
-    states[e.id] = Object.assign(
-      {}, 
-      states[e.id], 
-      e, 
-      { n_neighbors: states[e.id].neighbors.length, center: path.centroid(e) }
-    )
+    states[e.id] = Object.assign({}, states[e.id], e, { n_neighbors: states[e.id].neighbors.length, center: path.centroid(e) })
   })
   for(var k in states){
     const s = states[k];
-    //https://gist.github.com/conorbuck/2606166
     s.dir_neighbors = s.neighbors.length 
       ? s.neighbors
         .map(o => Math.atan2(states[o].center[1] - s.center[1], states[o].center[0] - s.center[0]) * 180 / Math.PI) 
         .map(o => o < 0 ? 360 + o : o)
       : []
   }
-  
-  var detailMap = svg.append("g")
-    .attr("class","detail-map")
-    .attr("transform","translate(1200,400)");
 
   var countryMap = svg.append("g")
     .attr("class","country-map")
-    .attr("transform","translate(0,100)")
-    .selectAll(".state")
+    .selectAll(".country-state")
     .data(Object.values(states));
   var countryGroups = countryMap.enter().append("g")
-    .attr("class","state")
-    .attr("transform", d => "translate("+d.center[0]+","+d.center[1]+")" );
-  var countryStates = countryGroups
-    .append("polygon")
-    .attr("points",d => createPointsPoly(d.n_neighbors, 20).map(o => o.join(",")).join(" "))
+    .attr("class","country-state")
+    .attr("transform", d => "translate("+d.center[0]+","+d.center[1]+")" )
     .on('click', function(d){
       current = d.id;
-      drawDetailMap()
+      drawDetailMap();
     });
+  countryGroups
+    .append("polygon")
+    .attr("points",d => createPointsPoly(d.n_neighbors, 20).map(o => o.join(",")).join(" "));
+  countryGroups
+    .append("text")
+    .attr("x", -10)
+    .attr("y", 5)
+    .text(d => d.abbr)
   
   function drawDetailMap(){
 
+    const t = d3.transition()
+      .duration(5000);
     var currentState = states[current]
-    var neighbors = [...states[current].neighbors]
+    var neighbors = [...currentState.neighbors]
       .sort((a,b) => 
         currentState.dir_neighbors[currentState.neighbors.indexOf(a)]
         - currentState.dir_neighbors[currentState.neighbors.indexOf(b)]
       );
-    const bigStateRadius = 100;
     const bigStateSideLen = getSideGivenRadius(neighbors.length, bigStateRadius);
 
-    var bigStateData = detailMap.selectAll(".big-state")
-      .data([currentState], d => d.id)
-    bigStateData.exit().remove();
-    var bigState = bigStateData
-      .enter().append("polygon")
-      .attr("class","big-state")
-      .attr("points",d => createPointsPoly(d.n_neighbors, 100).map(o => o.join(",")).join(" "));
+    var detailMapData = svg.selectAll(".detail-map")
+      .data([currentState], d => d.id);
+    detailMapData.exit().transition(t).style("opacity",0).remove();
+    var detailMap = detailMapData
+      .enter().append("g")
+      .attr("class","detail-map");
+
+    var bigState = detailMap.append("g")
+      .attr("class","detail-state big-state")
+      .attr("transform", d => "translate("+d.center[0]+","+d.center[1]+")");
+    bigState.append("polygon")
+      .attr("points",d => createPointsPoly(d.n_neighbors, littleStateRadius).map(o => o.join(",")).join(" "));
+    bigState.append("text")
+      .attr("x", -10)
+      .attr("y", 5)
+      .text(d => d.abbr)
+    bigState.transition(t)
+      .attr("transform","translate(0,0)")
+    bigState.select("polygon")
+      .transition(t)
+      .attr("points",d => createPointsPoly(d.n_neighbors, bigStateRadius).map(o => o.join(",")).join(" "));
 
     var neighborPolyData = detailMap
-      .selectAll(".neighbor")
+      .selectAll(".neighbor-state")
       .data(neighbors, d => d.id);
-    neighborPolyData.exit().remove();
     var neighborPoly = neighborPolyData
-      .enter().append("polygon")
-      .attr("class","neighbor");
+      .enter().append("g")
+      .attr("class","detail-state neighbor-state")
+      .on('click', function(d){
+        current = d;
+        drawDetailMap();
+      });
     neighborPoly
       .merge(neighborPolyData)
+      .attr("transform", d => "translate("+states[d].center[0]+","+states[d].center[1]+")")
+      .transition(t)
       .attr("transform", (d,i) => 
         getPositionBySide(i, currentState.n_neighbors, bigStateRadius, states[d].n_neighbors, bigStateSideLen))
+    neighborPoly.append("polygon")
+      .attr("points", d => createPointsPoly(states[d].n_neighbors, littleStateRadius))
+      .transition(t)
       .attr("points", d => createPointsPoly(
         states[d].n_neighbors, 
         getRadiusGivenSide(states[d].n_neighbors, bigStateSideLen)
       ));
-  }
 
+    detailMap.selectAll(".neighbor-state")
+      .append("text")
+      .attr("x", -10)
+      .attr("y", 5)
+      .attr("transform", (d,i) => 
+        "rotate("+ -180/pi * getRotateAngle(getAngleBySide(i, neighbors.length), states[d].n_neighbors)+")")
+      .text(d => states[d].abbr)
+
+    detailMap
+      .transition(t)
+      .attr("transform","translate(1150, 200)")
+  }
 }
 
-  
-//https://gist.github.com/davidegazze/56bc005090be06ccf545
 function createPointsPoly(n, radius) {
     var base = 2 * pi / n;
     var poly = new Array();
-    
     for(var i = 0; i < n; i++) {
-        var fi_i = base * i + 0; //set origin
+        var fi_i = base * i + 0;
         poly.push([
           radius * Math.cos(fi_i),
           radius * Math.sin(fi_i) 
@@ -117,9 +140,7 @@ function getPositionBySide(index, totalNeighbors, bigStateRadius, nSides, bigSta
   var bigStateApothem = getApothemGivenRadius(totalNeighbors, bigStateRadius);
   var x = Math.cos(angle)*(bigStateApothem + littleStateApothem);
   var y = Math.sin(angle)*(bigStateApothem + littleStateApothem);
-  var ret = "translate("+x+","+y+")"
-    + " rotate("+(180/pi * rotateAngle)+")";
-  return ret;
+  return "translate("+x+","+y+")" + " rotate("+(180/pi * rotateAngle)+")";
 }
 
 function getAngleBySide(index, totalNeighbors){
